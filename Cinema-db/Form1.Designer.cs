a7fa55f0fe89a6543e761ca289654022581809ec
ripmyloven <martinsild.mr@gmail.com>
2024-11-28 09:56:19 +0200

    git fix

--- Cinema-db/Form1.Designer.cs
+++ Cinema-db/Form1.Designer.cs
@@ -1,39 +1,32 @@
-﻿namespace Cinema_db
+﻿using System;
+using System.Windows.Forms;
+
+namespace Cinema_db
 {
-    partial class Form1
+    public partial class Form1 : Form  // Наследуем от Form
     {
-        /// <summary>
-        ///  Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        ///  Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
             {
                 components.Dispose();
             }
-            base.Dispose(disposing);
+            base.Dispose(disposing);  // Важно вызвать Dispose базового класса
         }
 
-        #region Windows Form Designer generated code
-
-        /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Text = "Form1";  // Установка текста формы
         }
-
-        #endregion
     }
 }
--- Cinema-db/Form1.cs
+++ Cinema-db/Form1.cs
@@ -1,15 +1,16 @@
-namespace Cinema_db
-{
-    public partial class Form1 : Form
-    {
-        public Form1()
-        {
-            InitializeComponent();
-        }
+using Microsoft.Data.SqlClient;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
 
-        private void createdb()
-        {
-            
-        }
+public class Form1 : Form
+{
+    SqlConnection conn = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\ripmy\source\repos\Cinema-db\cinemadb.mdf;Integrated Security=True");
+    public Form1()
+    {
+
     }
 }
